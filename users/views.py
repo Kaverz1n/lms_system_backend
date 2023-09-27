efa0d120568ee37167a2ddac0748d574bb715c65
Dmitry <dima.captan@yandex.ru>
2023-09-27 18:57:28 +0300

    added UserUpdateAPIView

---
+++ users/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    '''
+    Generic-класс для обновления пользователя
+    '''
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
