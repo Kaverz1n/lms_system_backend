53ca026cacbbf3f320add230820c95c8a9803353
Dmitry <dima.captan@yandex.ru>
2023-09-30 15:56:42 +0300

    registered model Payment

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'date', 'amount', 'method',)
+    list_filter = ('date', 'method',)
+    search_fields = ('user', 'course', 'lesson',)
+    ordering = ('-date',)
