dbd6ec14019dd8df6fdd48fb98c316e7ac2ab096
Dmitry <dima.captan@yandex.ru>
2023-09-30 15:58:04 +0300

    added new lesson-fields and create func in CourseSerializer

--- school/serializers.py
+++ school/serializers.py
@@ -3,16 +3,6 @@ from rest_framework import serializers
 from school.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    '''
-    Сериализатор для модели обучающего курса
-    '''
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
 class LessonSerializer(serializers.ModelSerializer):
     '''
     Сериализатор для модели обучающего курса
@@ -21,3 +11,34 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    '''
+    Сериализатор для модели обучающего курса
+    '''
+    lessons_quantity = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = (
+            'title',
+            'description',
+            'image',
+            'lessons_quantity',
+            'lessons',
+        )
+
+    def get_lessons_quantity(self, instance):
+        return Lesson.objects.filter(course=instance).count()
+
+    def create(self, validated_data):
+        print(validated_data)
+        lessons = validated_data.pop('lessons')
+        course = Course.objects.create(**validated_data)
+
+        for lesson in lessons:
+            Lesson.objects.create(**lesson, course=course)
+
+        return course
