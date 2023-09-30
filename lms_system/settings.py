50599b10cec7424f8da7ac8efa86c3c15a311965
Dmitry <dima.captan@yandex.ru>
2023-09-30 15:59:18 +0300

    added django_filters and payments in INSTALLED_APPS

--- lms_system/settings.py
+++ lms_system/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
 
     'rest_framework',
 
     'school',
     'users',
+    'payments',
 ]
 
 MIDDLEWARE = [
