39290d376a603f8667f7c156cec8fcb8199242e4
Dmitry <dima.captan@yandex.ru>
2023-09-30 16:00:10 +0300

    added PaymentListAPIView

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.filters import OrderingFilter
+
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    '''
+    Generic-класс для просмотра всех объектов Payment
+    '''
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lesson', 'method',)
+    ordering_fields = ('date',)
