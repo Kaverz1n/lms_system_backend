e042617021f8d3cb374a6fc31b876c7cfc8bc641
Dmitry <dima.captan@yandex.ru>
2023-10-03 23:12:15 +0300

    added get_serializer_class in UserProfileRetrieveAPIView

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
 
 from users.models import User
-from users.serializers import UpdateUserSerializer, UserPaymentSerializer
+from users.serializers import (
+    UpdateUserSerializer,
+    UserProfileSerializer,
+    UserProfileNoDataSerializer
+)
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
@@ -12,9 +16,14 @@ class UserUpdateAPIView(generics.UpdateAPIView):
     queryset = User.objects.all()
 
 
-class UserPaymentListAPIView(generics.ListAPIView):
+class UserProfileRetrieveAPIView(generics.RetrieveAPIView):
     '''
     Generic-класс для обновления пользователя
     '''
-    serializer_class = UserPaymentSerializer
+    serializer_class = UserProfileSerializer
     queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.user.pk == self.get_object().pk:
+            return UserProfileSerializer
+        return UserProfileNoDataSerializer
