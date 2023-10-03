2c450c04fd9146e10f58301c0a6a5db8b74f612b
Dmitry <dima.captan@yandex.ru>
2023-10-03 23:11:09 +0300

    path payments chaged to profile

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,16 @@ from django.urls import path
 from users.apps import UsersConfig
 from users.views import UserUpdateAPIView, UserPaymentListAPIView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
-    path('payments/', UserPaymentListAPIView.as_view(), name='user_payments_list'),
+    path('profile/<int:pk>/', UserPaymentListAPIView.as_view(), name='user_payments_list'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh')
 ]
