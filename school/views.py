5352fcb6bacf4fe4d050fbbfc2c3af7f7a9a4ca2
Dmitry <dima.captan@yandex.ru>
2023-10-07 14:41:03 +0300

    added SubscriptionCreateAPIView and SubscriptionDestroyAPIView

--- school/views.py
+++ school/views.py
@@ -1,18 +1,24 @@
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 
-from school.models import Course, Lesson
+from school.models import Course, Lesson, Subscription
+from school.paginator import CoursePaginator, LessonPaginator
 from school.permissions import IsModeratorOrOwner
-from school.serializers import CourseSerializer, LessonSerializer
+from school.serializers import CourseSerializer, LessonSerializer, SubscriptionSerializer, UserCourseSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     '''
     ViewSet для модели обучающего курса
     '''
-    serializer_class = CourseSerializer
     queryset = Course.objects.all()
     permission_classes = [IsAuthenticated, IsModeratorOrOwner]
+    pagination_class = CoursePaginator
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return UserCourseSerializer
+        return CourseSerializer
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
@@ -38,7 +44,7 @@ class LessonListAPIView(generics.ListAPIView):
     '''
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-
+    pagination_class = LessonPaginator
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     '''
@@ -55,3 +61,25 @@ class LessonDestroyAPIView(generics.DestroyAPIView):
     '''
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated, IsModeratorOrOwner]
+
+
+class SubscriptionCreateAPIView(generics.CreateAPIView):
+    '''
+    Generic-класс для создания подписки на курс
+    '''
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+
+class SubscriptionDestroyAPIView(generics.DestroyAPIView):
+    '''
+    Generic-класс для удаления подписки на курс
+    '''
+    queryset = Subscription.objects.all()
+
+    def get_object(self):
+        course_pk = self.kwargs.get('course_pk')
+        course = Course.objects.get(pk=course_pk)
+        object_ = Subscription.objects.get(course=course)
+
+        return object_
