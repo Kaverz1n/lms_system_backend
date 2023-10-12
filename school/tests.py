f9a29dfea80050ee94a51551ced0b391ae9e6d15
Dmitry <dima.captan@yandex.ru>
2023-10-12 17:27:44 +0300

    libraries are arranged alphabetically

--- school/tests.py
+++ school/tests.py
@@ -1,4 +1,5 @@
 from django.core.management import call_command
+
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import AccessToken
