7efe59c738034a70ac7b627bedfa6505c7d7d1aa
Dmitry <dima.captan@yandex.ru>
2023-10-12 17:28:39 +0300

    added new paths for doing payment

--- payments/urls.py
+++ payments/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from payments.apps import PaymentsConfig
-from payments.views import PaymentListAPIView
+from payments.views import PaymentListAPIView, PaymentCreateAPIView, PaymentIntentCreateAPIView
 
 app_name = PaymentsConfig.name
 
 urlpatterns = [
     path('', PaymentListAPIView.as_view(), name='payments_list'),
+    path('create/', PaymentCreateAPIView.as_view(), name='payment_create'),
+    path('<str:subject_type>/<int:subject_pk>/pay/', PaymentIntentCreateAPIView.as_view(), name='payment_intent_create')
 ]
