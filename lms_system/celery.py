f8a925f6d58c599053c4fa78b755536f85740453
Dmitry <dima.captan@yandex.ru>
2023-10-16 18:21:31 +0300

    added celery.py with celery settings

---
+++ lms_system/celery.py
@@ -0,0 +1,11 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms_system.settings')
+
+app = Celery('lms_system')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
